707510795e81535619fc32686f0573ad32f0becb
Devin Kopp <devinkopp@gwu.edu>
2018-10-19 03:08:56 -0400

    Fix iterative image manager looking for cfg paths

--- utils/iterative-image-manager.py
+++ utils/iterative-image-manager.py
@@ -1,15 +1,19 @@
 # Class that will handle moving the images between iterations
-import config
 import glob
 import os
 import sys
 import shutil
 
+im_path = "../images"
+sample_path = "../iterative_labeler/samples/"
+initial_train_images_path = sample_path + "initial_train_images/"
+test_im_dir = sample_path + "test_im_dir/"
+
 if __name__ == "__main__":
-	n_iterations = config.iterations + 1	# Add 1 for initial java training
+	n_iterations = 3	# Add 1 for initial java training
 	curr_iteration = int(sys.argv[1])
 
-	all_im_list = glob.glob(os.path.join(config.im_path,"*"))
+	all_im_list = glob.glob(os.path.join(im_path,"*"))
 
 	# Get number of images per iteration batch
 	iteration_batch_sz = int(1.0/n_iterations * len(all_im_list) + 0.01)
@@ -23,16 +27,16 @@ if __name__ == "__main__":
 
 	# Give 1 batch images to the java initial labeler (images)
 	if curr_iteration == 0:
-		target_dir = config.initial_train_images_path[:-1]
+		target_dir = initial_train_images_path[:-1]
 	else:
 		# Give 1 batch images to python tester (test_images)
 		# ^ Loop this for `iterations`
-		for file in os.listdir(config.test_im_dir):
-			fp = os.path.join(config.test_im_dir,file)
+		for file in os.listdir(test_im_dir):
+			fp = os.path.join(test_im_dir,file)
 			os.unlink(fp)
 
-		target_dir = config.test_im_dir[:-1]
+		target_dir = test_im_dir[:-1]
 
 	for i in range(i_start, i_end):
-		new_path = all_im_list[i].replace(config.im_path[:-1], target_dir)
+		new_path = all_im_list[i].replace(im_path[:-1], target_dir)
 		shutil.copyfile(all_im_list[i], new_path)
